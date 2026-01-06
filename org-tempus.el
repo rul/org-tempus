b046645918330e788a6845310e81a325d23855dd
Raúl Benencia <id@rbenencia.name>
2026-01-06 09:45:27 -0300

    Add package commentary

--- org-tempus.el
+++ org-tempus.el
@@ -1,6 +1,6 @@
-;;; org-tempus.el --- Tracks work sessions and breaks at a glance  -*- lexical-binding: t; -*-
+;;; org-tempus.el --- Enhance Org time tracking  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025  Raul Benencia
+;; Copyright (C) 2025-2026  Raul Benencia
 
 ;; Author: Raul Benencia <id@rbenencia.name>
 ;; Maintainer: Raul Benencia <id@rbenencia.name>
@@ -26,7 +26,17 @@
 
 ;;; Commentary:
 ;;
-;; TODO
+;; Org Tempus augments Org's native time tracking capabilities by
+;; setting customizable thresholds for a session, and computing the
+;; total clocked time in a day.  The package will optionally send
+;; notifications when thresholds are reached.  It can also detect
+;; activity and auto clock in to a default task, or auto clock out
+;; when idle.  The idle detection mechanism supports multiple sources,
+;; such as Emacs itself, Mutter (for GNOME), and freedesktop.org
+;; ScreenSaver.  Additionally, all the logged time and thresholds are
+;; displayed in the mode line.  This information can optionally be
+;; sent somewhere else (such as dconf, to integrate with a GNOME
+;; desktop environment, for example).
 
 ;;; Code:
 (require 'org-clock)
